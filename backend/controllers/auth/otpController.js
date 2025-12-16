97421b585dfb9177a8fab1044eded538a773903a
Ardy <148834227+CakArdy72@users.noreply.github.com>
2025-12-16 20:43:19 +0700

    Controller Otp Email

--- backend/controllers/auth/otpController.js
+++ backend/controllers/auth/otpController.js
@@ -1,1 +1,33 @@
+// controllers/auth/otpController.js
+const { User, Otp } = require("../../models");
+
+exports.verifyOtp = async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) return res.status(404).json({ message: "User tidak ditemukan" });
+
+    const existingOtp = await Otp.findOne({
+      where: {
+        userId: user.id,
+        code: otp
+      }
+    });
+
+    if (!existingOtp)
+      return res.status(400).json({ message: "Kode OTP tidak valid" });
+
+    if (existingOtp.expiredAt < new Date())
+      return res.status(400).json({ message: "Kode OTP telah kadaluarsa" });
+
+    user.emailVerified = new Date();
+    await user.save();
+    await existingOtp.destroy();
+
+    res.json({ success: true, message: "Email berhasil terverifikasi!" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
